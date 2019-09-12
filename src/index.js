6668db3936fa2df883c2665e126b9ba96d7ee50c
Daniel Gading <dgading@gmail.com>
2019-09-12 16:48:53 -0400

    Initial hoc test

--- src/index.js
+++ src/index.js
@@ -27,3 +27,4 @@ export { default as Table } from "./components/Table";
 export { default as Text } from "./components/Text";
 export { default as Title } from "./components/Title";
 export { default as Tags } from "./components/Tags";
+export { default as withResource } from "./components/Resources/withResource";
